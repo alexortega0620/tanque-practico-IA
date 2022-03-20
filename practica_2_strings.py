e5e4ab9bb3ff64564a1f1796ec1f426314d8f363
alexortega0620 <101947138+alexortega0620@users.noreply.github.com>
2022-03-20 16:51:12 -0600

    Add files via upload

---
+++ practica_2_strings.py
@@ -0,0 +1,12 @@
+
+"esto es un string"
+'esto tambien es un string'
+
+
+variable= '"Esto es un string con comillas dobles"'
+variable2="'Esto es otro string con comillas simples'"
+
+print("FRANCISCO ALEJANDRO ORTEGA GUZMAN 19310472\n")
+print(variable)
+print(variable2)
+
